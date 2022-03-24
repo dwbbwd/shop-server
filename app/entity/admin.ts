7bb4b4c4dd9d66872a25862a2d40d7fcd6815c90
邓文彬 <1051174750@qq.com>
2022-03-24 21:45:08 +0800

    feat: 添加管理员表

---
+++ app/entity/admin.ts
@@ -0,0 +1,53 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export default class Admin {
+    @PrimaryGeneratedColumn({
+        type: 'int',
+        comment: '主键id'
+    })
+    id: number;
+
+    @Column({
+        type: 'varchar',
+        length: 50,
+        nullable: false,
+        comment: '密码'
+    })
+    password: string;
+    @Column({
+        type: 'varchar',
+        length: 50,
+        nullable: false,
+        comment: '账号'
+    })
+    account: string;
+    @Column({
+        type: 'varchar',
+        length: 50,
+        nullable: false,
+        comment: '头像'
+    })
+    img: string;
+    @Column({
+        type: 'varchar',
+        length: 50,
+        nullable: false,
+        comment: '用户名'
+    })
+    username: string;
+
+    @Column({
+        type: 'int',
+        nullable: false,
+        comment: '创建时间'
+    })
+    createTime: number;
+
+    @Column({
+        type: 'int',
+        nullable: false,
+        comment: '修改时间'
+    })
+    updateTime: number;
+}
